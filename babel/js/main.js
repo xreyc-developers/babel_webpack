f54b8a47f5f2b1b15c509c9b44230f34840a6dd2
xreyc <segumareyco@gmail.com>
2021-10-02 12:41:09 +0800

    babel

--- babel/js/main.js
+++ babel/js/main.js
@@ -1,4 +1,18 @@
 const getUserId = username => {
-    if (username == 'xreyc') return 1;
-    if (username == 'kezeah') return 2;
+  if (username == 'xreyc') return 1;
+  if (username == 'kezeah') return 2;
 };
+
+fetch('coffee.jpg').then(response => {
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+  return response.blob();
+}).then(myBlob => {
+  let objectURL = URL.createObjectURL(myBlob);
+  let image = document.createElement('img');
+  image.src = objectURL;
+  document.body.appendChild(image);
+}).catch(e => {
+  console.log('There has been a problem with your fetch operation: ' + e.message);
+});
